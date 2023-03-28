de42ca5d1e7c2c3c263e465f0885a43d1a53c64b
Ali Raza <aliraza021419@gmail.com>
2023-03-28 19:21:20 -0400

    Added a button and multiple functions to change and replace grid layout based on user input and edited createGrid function to properly adjust to new  grid size in the same total space as the previous grid

--- script.js
+++ script.js
@@ -1,16 +1,56 @@
 const container = document.querySelector('.container');
 
-// Function to Create Each Grid Square // 
-// Added Event Listener to each grid square to change color when hovering over with the mouse //
-function createGrid() {
-    for (let i = 1; i <= 256; i++) {
-        const div = document.createElement('div');
-        div.classList.add('square');
-        div.addEventListener("mouseover", () => {
-            div.classList.add('color');
-        });
-        container.appendChild(div);
+// Allow User Input to Generate New Grid Size //
+function changeGrid() {
+    let input = prompt("Enter the New Grid Layout (1-100)");
+
+    while (Number.isNaN(Number(input)) || input < 1 || input > 100) {
+        input = prompt("Please Enter a Valid Input (1-100)");
+    }
+    return Number(input);
+    
+}
+
+// Replace Current Grid with New Grid //
+function replaceGrid() {
+    removeGrid();
+    const newRows = changeGrid();
+    createGrid(newRows);
+}
+
+// Remove the Current Grid by Looping Through Each Row//
+function removeGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
     }
 }
 
-createGrid();
+// Function to Create Each Grid Square // 
+// Sets Grid Square Height and Width Based off Grid Dimensions //
+function createGrid(rows) {
+    const cellDims = 650 / rows;
+
+    for (let i = 1; i <= rows; i++) {
+        const row = document.createElement('div')
+        row.classList.add('row');
+        row.setAttribute('id', `row-${i}`);
+
+        for (let j = 1; j <= rows; j++) {
+            const div = document.createElement('div');
+            div.classList.add('square');
+            div.style.cssText = `width: ${cellDims}px; height: ${cellDims}px;`;
+            addColor(div);
+            row.appendChild(div);
+        }
+        container.appendChild(row);
+    }
+}
+
+// Adds EventListener to each Grid Square that applies color when Hovering with the Mouse //
+function addColor(gridSquare) {
+    gridSquare.addEventListener("mouseover", () => {
+        gridSquare.classList.add('color');
+    });
+}
+
+createGrid(16);
