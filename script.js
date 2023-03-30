b14cbf26aeb2aabd8c78416fa803660f34d85e15
Ali Raza <aliraza021419@gmail.com>
2023-03-30 15:50:32 -0400

    Added an array to store each div square from the grid to allow easier access to each grid item for when wanting to apply color. Also added a color wheel for user to select color

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const container = document.querySelector('.container');
-const rows = container.children;
 const resetBtn = document.querySelector('.reset-btn');
 const blackBtn = document.querySelector('.black-btn');
+const randomBtn = document.querySelector('.random-btn');
 const colorBtn = document.querySelector('.color-btn');
+
+// Store every div square in array for easy access in changing each div color
+let gridSquares = [];
 
 
 // Allow User Input to Generate New Grid Size //
@@ -33,7 +36,8 @@ function removeGrid() {
 // Function to Create Each Grid Square // 
 // Sets Grid Square Height and Width Based off Grid Dimensions //
 function createGrid(rows) {
-    const cellDims = 650 / rows;
+    gridSquares.length = 0;  // resets div squares array to 0 for whenever we want to change grid layout
+    const cellDims = 650 / rows;  // 650 is the width and height of the container
 
     for (let i = 1; i <= rows; i++) {
         const row = document.createElement('div')
@@ -44,6 +48,7 @@ function createGrid(rows) {
             const div = document.createElement('div');
             div.classList.add('square');
             div.style.cssText = `width: ${cellDims}px; height: ${cellDims}px;`;
+            gridSquares.push(div);
             row.appendChild(div);
         }
         container.appendChild(row);
@@ -52,35 +57,16 @@ function createGrid(rows) {
 
 // Adds EventListener to each Grid Square that applies a random color when Hovering with the Mouse //
 function addRandomColor() {
-    for (let i = 0; i < rows.length; i++) {
-        let row = rows[i];
-        let gridSquares = row.children;
-        for (let j = 0; j < gridSquares.length; j++) {
-            let gridSquare = gridSquares[j];
-            gridSquare.addEventListener("mouseover", () => {
-                let randomColors = changeColor();
-                gridSquare.style.backgroundColor = `rgb(${randomColors[0]}, ${randomColors[1]}, ${randomColors[2]})`;
-            });
-        }
-    }
-}
-
-// Adds EventListener to each Grid Square that applies a black color when Hovering with the Mouse //
-function addBlackColor() {
-    for (let i = 0; i < rows.length; i++) {
-        let row = rows[i];
-        let gridSquares = row.children;
-        for (let j = 0; j < gridSquares.length; j++) {
-            let gridSquare = gridSquares[j];
-            gridSquare.addEventListener("mouseover", () => {
-               gridSquare.style.backgroundColor = 'black'; 
-            });
-        }
-    }
+    gridSquares.forEach((square) => {
+        square.addEventListener("mouseover", () => {
+            let randomColors = getRandomColor();
+            square.style.backgroundColor = `rgb(${randomColors[0]}, ${randomColors[1]}, ${randomColors[2]})`;
+    });
+});
 }
 
 // Selects 3 random values to be used for RGB color //
-function changeColor() {
+function getRandomColor() {
     const randomColors = [];
 
     for (let i = 1; i <= 3; i++) {
@@ -89,18 +75,33 @@ function changeColor() {
     }
     return randomColors;
 }
+    
 
-// Add Event Listener to Reset Button to Remove all color from the Grid //
-// Loops through the child of each row to access each individual grid square //
+// Adds EventListener to each Grid Square that applies a black color when Hovering with the Mouse //
+function addBlackColor() {
+    gridSquares.forEach((square) => {
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = 'black'; 
+    });
+});
+}
+
+// Function to pick color from color wheel //
+function pickColor() {
+    const colorValue = document.getElementById("colorpicker").value;
+    
+    gridSquares.forEach((square) => {
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = `${colorValue}`;
+    });
+});
+}
+
+// Reset Button to Remove all color from the Grid //
 function removeColor() {
-    for (let i = 0; i < rows.length; i++) {
-        let row = rows[i];
-        let gridSquares = row.children;
-        for (let j = 0; j < gridSquares.length; j++) {
-            let gridSquare = gridSquares[j];
-            gridSquare.style.backgroundColor = 'inherit';
-        }
-    }
+    gridSquares.forEach((square) => {
+        square.style.backgroundColor = 'inherit'; 
+    });
 }
 
 createGrid(16);
